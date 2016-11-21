03ed9a30b3ec6377a14f351b7e9d6038eca1f542
jmacato <hikari.netto23@gmail.com>
2016-11-21 12:44:19 +0800

    気にしないよこれわ～

--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
+using System.Diagnostics;
 
 namespace LOLpreter
 {
@@ -59,6 +60,9 @@ namespace LOLpreter
             }
         }
 
+        /*
+        Definitions of each tokens/lexemes
+        */
         Dictionary<string, string> LexemeDefinitions = new Dictionary<string, string>
         {
             {@"HAI","Delimiter to mark the start of the program"},
@@ -73,61 +77,92 @@ namespace LOLpreter
             {@"ITZ","Assignment operator in declaring a variable"},
             {@"GIMMEH","Input"},
             {@"VISIBLE","Output"},
-            {@"BOTH SAEM","Comparison Operator; True if operands are equal"},
-            {@"DIFFRINT"," Comparison Operator; True if operands are not equal"},
-            {@"SUM OF","Arithmetic Operator; Adds operands"},
-            {@"DIFF OF","Arithmetic Operator; Subtracts operand"},
-            {@"PRODUKT OF","Arithmetic Operator; Multiplies operands"},
-            {@"QUOSHUNT OF","Arithmetic Operator; Divides operands"},
-            {@"MOD OF","Arithmetic Operator; Returns the remainder of the operands"},
-            {@"BIGGR OF","Comparison Operator; Returns the biggest of the given integers"},
-            {@"SMALLR OF","Comparison Operator; Returns the smallest of the given integers"}, 
-            {@"O RLY?","If-Else Delimiter; Signals the start of the If-Else block"},
-            {@"YA RLY","If the expression provided in the If-Else block is true, the code in this block will be executed"}, 
-            {@"NO WAI","If the expression provided in the If-Else block is false, the code in this block will be executed. Also signals the end of the ​YA RLY​ block"},
             {@"OIC","Signals the end of the If-Else block"},
             {@"WTF[?]","Signals the start of a Switch Case block"},
             {@"OMG","Comparison block for a Switch Case. Followed by a literal"},
             {@"OMGWTF","The default optional case in a Switch Case block."},
-            {@"IM IN YR","Signals the start of a loop. Followed by a label."},
-            {@"IM OUTTA YR"," Signals the end of a loop.Followed by a label."},
-            {@"HOW IZ I"," Initializes a function. Followed by the function name."},
-            {@"IF U SAY SO","Closes a function block."},
-            {@"I IZ","Calls a function. Followed by the function name and its parameters."},
-            {@"GTFO","Break statement."}, 
-            {@"MEBBE","Appears between the ​YA RLY ​and ​NO WAI​ blocks. Similar to an Elseif statement."}, 
-            {@"AN","Separates arguments."}, 
-            {@"BOTH OF"," Boolean Operator; Similar to AND; 1 or 2 operands"}, 
-            {@"EITHER OF","Boolean Operator; Similar to OR; 1 or 2 operands"}, 
-            {@"WON OF","Boolean Operator; Similar to XOR; Infinite operands"}, 
-            {@"NOT","Negation"}, 
-            {@"ALL OF"," Boolean Operator; Similar to AND; Infinite operands"}, 
-            {@"ANY OF","Boolean Operator; Similar to OR; Infinite operands"}, 
-            {@"IS NOW A","Used for re-casting a variable to a different type"}, 
-            {@"MAEK","Used for re-casting a variable to a different type"}, 
-            {@"UPPIN","Increments a variable by one."}, 
-            {@"NERFIN","Decrements a variable by one."}, 
-            {@"FOUND YR","Returns the value of succeeding expression."}, 
+            {@"GTFO","Break statement."},
+            {@"MEBBE","Appears between the ​YA RLY ​and ​NO WAI​ blocks. Similar to an Elseif statement."},
+            {@"AN","Separates arguments."},
+            {@"NOT","Negation"},
+            {@"MAEK","Used for re-casting a variable to a different type"},
+            {@"UPPIN","Increments a variable by one."},
+            {@"NERFIN","Decrements a variable by one."},
             {@"MKAY","Delimiter of a function call."}, 
             {@"WILE","Evaluates an expression as a Boolean statement. If it evaluates to true, the execution is continued. Else, the execution stops."}, 
             {@"TILL","Evaluates an expression as a Boolean statement. If it evaluates to true, the execution is continued. Else, the execution stops."}, 
             {@"IT\s","Temporary variable. Remains in local scope until it is replaced with a bare expression."}, 
-            {@"SMOOSH","Expects strings as its input arguments for concatenation"} 
+            {@"SMOOSH","Expects strings as its input arguments for concatenation"},
+            {@"DIFFRINT"," Comparison Operator; True if operands are not equal"},
+            {@"BOTH-SAEM","Comparison Operator; True if operands are equal"},
+            {@"SUM-OF","Arithmetic Operator; Adds operands"},
+            {@"DIFF-OF","Arithmetic Operator; Subtracts operand"},
+            {@"PRODUKT-OF","Arithmetic Operator; Multiplies operands"},
+            {@"QUOSHUNT-OF","Arithmetic Operator; Divides operands"},
+            {@"MOD-OF","Arithmetic Operator; Returns the remainder of the operands"},
+            {@"BIGGR-OF","Comparison Operator; Returns the biggest of the given integers"},
+            {@"SMALLR-OF","Comparison Operator; Returns the smallest of the given integers"},
+            {@"O-RLY?","If-Else Delimiter; Signals the start of the If-Else block"},
+            {@"YA-RLY","If the expression provided in the If-Else block is true, the code in this block will be executed"},
+            {@"NO-WAI","If the expression provided in the If-Else block is false, the code in this block will be executed. Also signals the end of the ​YA RLY​ block"},
+            {@"IM-IN-YR","Signals the start of a loop. Followed by a label."},
+            {@"IM-OUTTA-YR"," Signals the end of a loop.Followed by a label."},
+            {@"HOW-IZ-I"," Initializes a function. Followed by the function name."},
+            {@"IF-U-SAY-SO","Closes a function block."},
+            {@"I-IZ","Calls a function. Followed by the function name and its parameters."},
+            {@"BOTH-OF"," Boolean Operator; Similar to AND; 1 or 2 operands"},
+            {@"EITHER-OF","Boolean Operator; Similar to OR; 1 or 2 operands"},
+            {@"WON-OF","Boolean Operator; Similar to XOR; Infinite operands"},
+            {@"ALL-OF"," Boolean Operator; Similar to AND; Infinite operands"},
+            {@"ANY-OF","Boolean Operator; Similar to OR; Infinite operands"},
+            {@"IS-NOW-A","Used for re-casting a variable to a different type"},
+            {@"FOUND-YR","Returns the value of succeeding expression."}
+        };
+
+        /*
+        Since we split tokens by spaces
+        It's necessary to turn multiple space operators into
+        a single connected string
+        */
+        Dictionary<string, string> MonoglyphyOperators = new Dictionary<string, string>
+        {
+            {@"BOTH SAEM","BOTH-SAEM"},
+            {@"SUM OF","SUM-OF"},
+            {@"DIFF OF","DIFF OF"},
+            {@"PRODUKT OF","PRODUKT-OF"},
+            {@"QUOSHUNT OF","QUOSHUNT-OF"},
+            {@"MOD OF","MOD-OF"},
+            {@"BIGGR OF","BIGGR-OF"},
+            {@"SMALLR OF","SMALLR-OF"},
+            {@"O RLY?","O RLY?"},
+            {@"YA RLY","YA-RLY"},
+            {@"NO WAI","NO-WAI"},
+            {@"IM IN YR","IM-IN-YR"},
+            {@"IM OUTTA YR","IM-OUTTA-YR"},
+            {@"HOW IZ I","HOW-IZ-I"},
+            {@"IF U SAY SO","IF-U-SAY-SO"},
+            {@"I IZ","I-IZ"},
+            {@"BOTH OF","BOTH-OF"},
+            {@"EITHER OF","EITHER-OF"},
+            {@"WON OF","WON-OF"},
+            {@"ALL OF","ALL-OF"},
+            {@"ANY OF","ANY-OF"},
+            {@"IS NOW A","IS-NOW-A"},
+            {@"FOUND YR","FOUND-YR"}
         };
 
         private void button2_Click(object sender, EventArgs e)
         {
             lolstream = textBox1.Text;
 
-            Regex SCRegex = new Regex(@"BTW(.*?)(\n|\r|\r\n)");
-            lolstream = SCRegex.Replace(lolstream, "");
-
+            //Regex SCRegex = new Regex(@"BTW(.*?)(\n|\r|\r\n)");
+            //lolstream = SCRegex.Replace(lolstream, "");
             int lineaddress = 0; //Actively parsed line
-            int iToken = 0; // Current Token
+            //int iToken = 0; // Current Token
             tableLayoutPanel1.Controls.Clear();
 
-            //Directives flags
-            double progVersion = 0;
+            ////Directives flags
+            //double progVersion = 0;
 
             //Split them 'up and remove extra lines
             string[] lollines = lolstream.Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
@@ -146,7 +181,7 @@ namespace LOLpreter
 
                     foreach (var result in results)
                     {
-                        Console.WriteLine(result.Value);
+                        Debug.WriteLine(s+"|"+result.Value);
                     }
 
 
@@ -185,10 +220,10 @@ namespace LOLpreter
                     {
 
                     }*/
-                    iToken += 1;
+                    //iToken += 1;
                 }
                 lineaddress += 1;
-                iToken = 0;
+                //iToken = 0;
             }
         }
 
